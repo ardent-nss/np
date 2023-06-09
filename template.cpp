eda79edcdd18ea7068ebef83c3c3ed7331adb15d
HackerDaGreat57 <HackerDaGreat57@gmail.com>
2023-06-09 11:46:57 -0700

    i work on too many projects :facepalm:

---
+++ template.cpp
@@ -0,0 +1,10 @@
+//main.cpp: this file is used to calibrate the flibbertigibbet
+
+//Includes
+#include <iostream> //Console streams (essential)
+
+int main(int argc, char *argv[]) {
+  //figure out how to hack into the fbi's nude collection here
+
+  return 0;
+}
