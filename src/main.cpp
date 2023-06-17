2c4ae41b8a4533cac2ee54a70e9cac9dd8c50326
Akshat Singh <HackerDaGreat57@gmail.com>
2023-06-17 14:44:07 -0700

    done implementing library support

---
+++ src/main.cpp
@@ -0,0 +1,10 @@
+//main.cpp: <describe what this file does>
+
+//Includes
+#include <iostream> //Console I/O streams
+
+int main(int argc, char *argv[]) {
+  //make stallman proud!
+
+  return 0;
+}
